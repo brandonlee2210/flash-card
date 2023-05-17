b7e348d67d89c1ff738c68633dbc9f9127f46480
brandon <brandonnlee2210@gmail.com>
2023-05-17 09:28:23 +0700

    Finish working on deck controllers

---
+++ server/src/controllers/createCardForDeckController.ts
@@ -0,0 +1,18 @@
+import { Request, Response } from "express";
+import Deck from "../models/Deck";
+
+export async function createCardForDeckController(req: Request, res: Response) {
+  const deckId = req.params.deckId;
+
+  try {
+    const deck = await Deck.findById(deckId);
+    if (!deck) return res.status(400).send("no deck of this id exists");
+    const { text } = req.body;
+    deck.cards.push(text);
+    await deck.save();
+    res.json(deck);
+  } catch (err) {
+    console.log(err);
+    res.status(500).send("something went wrong");
+  }
+}
